7dd009f787589e26a90204e64b97da3f4b038d31
blustring <rchlmdn83@gmail.com>
2023-05-19 10:41:46 +0200

    fixing small design in scss

--- frontend/src/lib/sanity/MyGamePage.js
+++ frontend/src/lib/sanity/MyGamePage.js
@@ -48,18 +48,24 @@ const MyGamePage = () => {
   }
 
   return (
-    <div>
+    <section id="gamePageAlone">
+    <div id="imgGamePage">
       <img src={fetchedGame.image} alt={fetchedGame.name} />
-        <p><b>Rating: </b>{game?.rating}</p>
-        <p><b>Plot:</b> {game?.description_raw}</p>
-        <p><b>Tags:</b> {game?.tags?.map((tag) => tag.name).join(", ")}</p>
-        <p><b>Genre:</b> {game?.genres?.map((genreList) => genreList.name).join(", ")}</p>
-        <p><b>Developer:</b> {fetchedGame.developer}</p>
-        <p><b>Publisher:</b> {game?.publishers?.map((publisher) => publisher.name).join(", ")}</p>
-        <p><b>Release date:</b> {game?.released}</p>
-        <p><b>Platforms:</b> {game?.platforms?.map((platform) => platform.platform.name).join(", ")}</p>
-        <p><b>Stores:</b> {game?.stores?.map((store) => store.store.name).join(", ")}</p>
     </div>
+
+    <div id="detailsGamePage">
+    <h1>{game?.name}</h1>
+      <p><b>Rating: </b>{game?.rating}</p>
+      <p><b>Plot:</b> {game?.description_raw}</p>
+      <p><b>Tags:</b> {game?.tags?.map((tag) => tag.name).join(", ")}</p>
+      <p><b>Genre:</b> {game?.genres?.map((genreList) => genreList.name).join(", ")}</p>
+      <p><b>Developer:</b> {fetchedGame.developer}</p>
+      <p><b>Publisher:</b> {game?.publishers?.map((publisher) => publisher.name).join(", ")}</p>
+      <p><b>Release date:</b> {game?.released}</p>
+      <p><b>Platforms:</b> {game?.platforms?.map((platform) => platform.platform.name).join(", ")}</p>
+      <p><b>Stores:</b> {game?.stores?.map((store) => store.store.name).join(", ")}</p>
+  </div>
+  </section>
   );
 };
 
